ae74204ded7b804f697c1e7eed13dadbff2efce3
NouiliKh <khalil.nouili@gmail.com>
2023-02-16 13:52:06 +0100

    start terraforming

---
+++ provision/main.tf
@@ -0,0 +1,23 @@
+terraform {
+  required_version = ">= 0.14"
+  required_providers {
+    google = "~> 3.6"
+  }
+}
+
+provider "google" {
+    project = var.project_id
+}
+
+data "google_project" "project" {
+    project_id = var.project_id
+}
+
+locals {
+    bucket_type = "REGIONAL"
+    region = var.region == null ? var.subnet_region : var.region
+}
+
+data "google_compute_zones" "available" {
+}
+
